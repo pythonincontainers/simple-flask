c23fbc232821b8e783c71fad5b59a9148d5a7201
python-in-containers <45329364+python-in-containers@users.noreply.github.com>
2019-02-14 14:40:56 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Flask Hello world! Version 1'
+
+@app.route('/test')
+def test():
+    return 'Testing hidden functionality ;)'
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
